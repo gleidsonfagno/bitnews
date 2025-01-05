b273bde8c1abf6a9291339c365b2b36f104024cf
gledsonfagno <gleidsonfagner838@gmail.com>
2025-01-05 14:33:26 -0300

    Deploy para vercel nava condiguraçao.1.2

--- build_files.sh
+++ build_files.sh
@@ -1,25 +1,15 @@
 #!/bin/bash
+
 echo "Iniciando o build_files.sh..."
 
-# Instalar Pip
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3.9 get-pip.py
-
-echo "Pip instalado, verificando versão..."
-pip --version
-
-# Ativar ambiente virtual, se necessário
-if [ -d "venv" ]; then
-    echo "Ativando ambiente virtual..."
-    source venv/bin/activate
+# Criar ou ativar ambiente virtual
+if [ ! -d "venv" ]; then
+    python3.9 -m venv venv
 fi
+source venv/bin/activate
 
-# Instalar pacotes necessários
-echo "Instalando pacotes do requirements.txt..."
+# Instalar dependências
+pip install --upgrade pip
 pip install -r requirements.txt
-
-# Coletar arquivos estáticos
-echo "Coletando arquivos estáticos..."
-python3.9 manage.py collectstatic --noinput
 
 echo "build_files.sh concluído."
