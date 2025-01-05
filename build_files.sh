0f20a0a6fe21a5354d7c8af7816658c57f9ba14c
gledsonfagno <gleidsonfagner838@gmail.com>
2025-01-05 14:16:27 -0300

    Deploy para vercel

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,11 @@
-# build_files.sh
+#!/bin/bash
+# Ativar ambiente virtual, se necessário
+if [ -d "venv" ]; then
+    source venv/bin/activate
+fi
+
+# Instalar pacotes necessários
 pip install -r requirements.txt
+
+# Coletar arquivos estáticos
 python3.9 manage.py collectstatic --noinput
